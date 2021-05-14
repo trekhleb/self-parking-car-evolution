bcbc2bb669f6dd6a463ad52e5267e63969381c98
Oleksii Trekhleb <trehleb@gmail.com>
2021-05-14 17:17:04 +0200

    Do smth.

--- src/components/Car/Chassis.tsx
+++ src/components/Car/Chassis.tsx
@@ -15,6 +15,10 @@ useGLTF.preload(modelPath);
 
 export interface ChassisProps extends BoxProps {
   weight?: number,
+  wireframe?: boolean,
+  castShadow?: boolean,
+  receiveShadow?: boolean,
+  styled?: boolean,
 }
 
 const onCollide = (e: any) => {
@@ -22,56 +26,143 @@ const onCollide = (e: any) => {
   console.log('Bonk!', e.body.userData)
 }
 
-function Beetle(props: GroupProps) {
+type BeetleProps = {
+  bodyProps?: GroupProps,
+  wireframe?: boolean,
+  castShadow?: boolean,
+  receiveShadow?: boolean,
+  styled?: boolean,
+};
+
+function Beetle(props: BeetleProps) {
+  const {
+    bodyProps = {},
+    wireframe = false,
+    styled = true,
+    castShadow = true,
+    receiveShadow = true,
+  } = props;
+
   const { nodes, materials }: ModelData = useGLTF(modelPath)
 
-  const g1 = nodes.chassis_1.geometry;
-  const g2 = nodes.chassis_2.geometry;
-  const g3 = nodes.chassis_3.geometry;
-  const g4 = nodes.chassis_4.geometry;
-  const g5 = nodes.chassis_5.geometry;
-  const g6 = nodes.chassis_6.geometry;
-  const g7 = nodes.chassis_7.geometry;
-  const g8 = nodes.chassis_8.geometry;
-  const g9 = nodes.chassis_9.geometry;
-  const g10 = nodes.chassis_10.geometry;
-  const g11 = nodes.chassis_11.geometry;
-  const g12 = nodes.chassis_12.geometry;
-  const g13 = nodes.chassis_12.geometry;
-  const g14 = nodes.chassis_14.geometry;
-  const g15 = nodes.chassis_15.geometry;
-  const g16 = nodes.chassis_16.geometry;
-
   return (
-    <group {...props} dispose={null}>
-      <mesh receiveShadow castShadow material={materials['Black paint']} geometry={g1} />
-      <mesh receiveShadow castShadow material={materials['Rubber']} geometry={g2} />
-      <mesh receiveShadow castShadow material={materials['Paint']} geometry={g3} />
-      <mesh receiveShadow castShadow material={materials['Underbody']} geometry={g4} />
-      <mesh receiveShadow castShadow material={materials['Chrom']} geometry={g5} />
-      <mesh receiveShadow castShadow material={materials['Interior (dark)']} geometry={g6} />
-      <mesh receiveShadow castShadow material={materials['Interior (light)']} geometry={g7} />
-      <mesh receiveShadow castShadow material={materials['Reflector']} geometry={g8} />
-      <mesh receiveShadow castShadow material={materials['Glass']} geometry={g9} />
-      <mesh receiveShadow castShadow material={materials['Steel']} geometry={g10} />
-      <mesh receiveShadow castShadow material={materials['Black plastic']} geometry={g11} />
-      <mesh receiveShadow castShadow material={materials['Headlight']} geometry={g12} />
-      <mesh receiveShadow castShadow material={materials['Reverse lights']} geometry={g13} />
-      <mesh receiveShadow castShadow material={materials['Orange plastic']} geometry={g14} />
-      <mesh receiveShadow castShadow material={materials['Tail lights']} geometry={g15} />
-      <mesh receiveShadow castShadow material={materials['License Plate']} geometry={g16} />
+    <group {...bodyProps} dispose={null}>
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Black paint']}
+        geometry={nodes.chassis_1.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Rubber']}
+        geometry={nodes.chassis_2.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Paint']}
+        geometry={nodes.chassis_3.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Underbody']}
+        geometry={nodes.chassis_4.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Chrom']}
+        geometry={nodes.chassis_5.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Interior (dark)']}
+        geometry={nodes.chassis_6.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Interior (light)']}
+        geometry={nodes.chassis_7.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Reflector']}
+        geometry={nodes.chassis_8.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Glass']}
+        geometry={nodes.chassis_9.geometry} />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Steel']}
+        geometry={nodes.chassis_10.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Black plastic']}
+        geometry={nodes.chassis_11.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Headlight']}
+        geometry={nodes.chassis_12.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Reverse lights']}
+        geometry={nodes.chassis_13.geometry} />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Orange plastic']}
+        geometry={nodes.chassis_14.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['Tail lights']}
+        geometry={nodes.chassis_15.geometry}
+      />
+      <mesh
+        receiveShadow={receiveShadow}
+        castShadow={castShadow}
+        material={materials['License Plate']}
+        geometry={nodes.chassis_16.geometry}
+      />
     </group>
   )
 }
 
 // The vehicle chassis
 const Chassis = forwardRef<THREE.Object3D | undefined, ChassisProps>((props, ref) => {
+  const {
+    rotation,
+    angularVelocity,
+    wireframe = false,
+    styled = true,
+    castShadow = true,
+    receiveShadow = true,
+    weight = 500,
+  } = props;
+
   const boxSize = [1.7, 1, 4] // roughly the cars' visual dimensions
   useBox(
     () => ({
-      mass: props.weight || 500,
-      rotation: props.rotation,
-      angularVelocity: props.angularVelocity,
+      mass: weight,
+      rotation,
+      angularVelocity,
       allowSleep: false,
       args: boxSize,
       onCollide,
@@ -84,9 +175,19 @@ const Chassis = forwardRef<THREE.Object3D | undefined, ChassisProps>((props, ref
     ref
   )
 
+  const bodyProps: GroupProps = {
+    position: [0, -0.6, 0],
+  };
+
   return (
     <mesh ref={ref}>
-      <Beetle position={[0, -0.6, 0]} />
+      <Beetle
+        bodyProps={bodyProps}
+        castShadow={castShadow}
+        receiveShadow={receiveShadow}
+        wireframe={wireframe}
+        styled={styled}
+      />
     </mesh>
   )
 })
