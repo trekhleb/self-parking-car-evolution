a0f53cc172fe77d8fe9577b5670d3a9b5d7af835
Oleksii Trekhleb <trehleb@gmail.com>
2021-05-22 19:54:31 +0200

    Do smth.

--- src/components/shared/MainNav.tsx
+++ src/components/shared/MainNav.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { NavLink } from 'react-router-dom';
 import { StyledLink } from 'baseui/link';
 
 import { routes } from '../../constants/routes';
+import { RiFilePaper2Fill, SiGithub } from 'react-icons/all';
 
 function MainNav() {
+  const linkStyle: CSSProperties = {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '20px',
+    marginBottom: '10px',
+  };
+
+  const iconStyle: CSSProperties = {
+    marginRight: '5px',
+  };
+
   return (
-    <nav>
-      <NavLink to={routes.home.path} component={StyledLink} exact>
-        Park by myself
-      </NavLink>
-      <NavLink to={routes.automaticParking.path} component={StyledLink} exact>
-        Teach car to park
-      </NavLink>
-    </nav>
+    <>
+      <nav style={{ display: 'flex', marginBottom: '20px' }}>
+        <StyledLink href="https://trekhleb.dev/blog/" style={linkStyle}>
+          <RiFilePaper2Fill style={iconStyle} /> How it woks
+        </StyledLink>
+        <StyledLink href="https://github.com/trekhleb/self-parking-car-evolution" style={linkStyle}>
+          <SiGithub style={iconStyle} /> Source-code
+        </StyledLink>
+      </nav>
+      <nav>
+        <NavLink to={routes.home.path} component={StyledLink} exact>
+          Park by myself
+        </NavLink>
+        <NavLink to={routes.automaticParking.path} component={StyledLink} exact>
+          Teach car to park
+        </NavLink>
+      </nav>
+    </>
   );
 }
 
