4599eacdb31fa5f8c2dc5ee6b910c18fe00952f0
Oleksii Trekhleb <trehleb@gmail.com>
2021-05-22 10:47:28 +0200

    Do smth.

--- src/components/shared/MainNav.tsx
+++ src/components/shared/MainNav.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { StyledLink } from 'baseui/link';
 
 import { routes } from '../../constants/routes';
 
 function MainNav() {
   return (
     <nav>
-      <NavLink to={routes.manualParking.path}>
-        Manual Parking
+      <NavLink to={routes.main.path} component={StyledLink} exact>
+        Park by myself
       </NavLink>
-      <NavLink to={routes.automaticParking.path}>
-        Automatic Parking
+      <NavLink to={routes.automaticParking.path} component={StyledLink} exact>
+        Teach car to park
       </NavLink>
     </nav>
   );
