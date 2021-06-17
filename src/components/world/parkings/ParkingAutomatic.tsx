47bbf7b2bf35823755d285ae2e5c47613b77a041
Oleksii Trekhleb <trehleb@gmail.com>
2021-06-17 09:55:06 -0400

    Do smth.

--- src/components/world/parkings/ParkingAutomatic.tsx
+++ src/components/world/parkings/ParkingAutomatic.tsx
@@ -4,6 +4,7 @@ import Ground from '../surroundings/Ground';
 import StaticCars from '../cars/StaticCars';
 import DynamicCars from '../cars/DynamicCars';
 import ParkingSpot from '../surroundings/ParkingSpot';
+import { CarsType } from '../types/car';
 
 // Collision groups and masks must be powers of 2.
 // @see: https://github.com/schteppe/cannon.js/blob/master/demos/collisionFilter.html
@@ -13,14 +14,14 @@ const COLLISION_MASK_ACTIVE_CARS = COLLISION_GROUP_STATIC_OBJECTS // It can only
 const COLLISION_MASK_STATIC_OBJECTS = COLLISION_GROUP_ACTIVE_CARS // It can only collide with active cars.
 
 type ParkingAutomaticProps = {
-  dynamicCarsNum?: number,
+  cars: CarsType,
   withVisibleSensors?: boolean,
   withLabels?: boolean,
 };
 
 function ParkingAutomatic(props: ParkingAutomaticProps) {
   const {
-    dynamicCarsNum = 10,
+    cars,
     withVisibleSensors = false,
     withLabels = false,
   } = props;
@@ -34,7 +35,7 @@ function ParkingAutomatic(props: ParkingAutomaticProps) {
       />
       <ParkingSpot />
       <DynamicCars
-        carsNum={dynamicCarsNum}
+        cars={cars}
         collisionFilterGroup={COLLISION_GROUP_ACTIVE_CARS}
         collisionFilterMask={COLLISION_MASK_ACTIVE_CARS}
         controllable
