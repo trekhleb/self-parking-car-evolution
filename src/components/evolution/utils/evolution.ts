fbbbbde4056fc0e242243574d28752b3c5cf701b
Oleksii Trekhleb <trehleb@gmail.com>
2021-06-24 15:38:01 +0200

    Do smth.

--- src/components/evolution/utils/evolution.ts
+++ src/components/evolution/utils/evolution.ts
@@ -1,5 +1,12 @@
 import { Generation, Genome } from '../../../lib/genetic';
-import { CarLicencePlateType, CarsType } from '../../world/types/car';
+import {
+  CarLicencePlateType,
+  CarsType,
+  EngineOptionsType,
+  SensorValuesType,
+  WheelOptionsType
+} from '../../world/types/car';
+import { SENSORS_NUM } from '../../world/car/constants';
 
 export const generateWorldVersion = (generationIndex: number | null, batchIndex: number | null): string => {
   const generation = generationIndex === null ? -1 : generationIndex;
@@ -15,10 +22,34 @@ export const generationToCars = (population: Generation): CarsType => {
   const cars: CarsType = {};
   population.forEach((genome: Genome, genomeIndex) => {
     const licencePlate = generateLicencePlate(genomeIndex);
+
+    const onEngine = (sensors: SensorValuesType): EngineOptionsType => {
+      const random = Math.random();
+      if (random < 0.3) {
+        return 'backwards';
+      }
+      if (random > 0.6) {
+        return 'forward'
+      }
+      return 'neutral';
+    };
+
+    const onWheel =  (sensors: SensorValuesType): WheelOptionsType => {
+      const random = Math.random();
+      if (random < 0.3) {
+        return 'left';
+      }
+      if (random > 0.6) {
+        return 'straight'
+      }
+      return 'right';
+    };
+
     cars[licencePlate] = {
       licencePlate,
-      onEngine: () => {},
-      onWheel: () => {},
+      sensorsNum: SENSORS_NUM,
+      onEngine,
+      onWheel,
       onHit: () => {},
       onMove: () => {},
     };
