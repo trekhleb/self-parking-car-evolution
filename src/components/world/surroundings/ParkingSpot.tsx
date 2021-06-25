5cfe1cc1c91742d15a7e93ffc93337ec6dab796c
Oleksii Trekhleb <trehleb@gmail.com>
2021-06-25 12:25:47 +0200

    Do smth.

--- src/components/world/surroundings/ParkingSpot.tsx
+++ src/components/world/surroundings/ParkingSpot.tsx
@@ -3,29 +3,34 @@ import { Line } from '@react-three/drei';
 
 import { CHASSIS_LENGTH, CHASSIS_WIDTH } from '../car/constants';
 import { NumVec3 } from '../types/vectors';
+import { Vector3 } from 'three';
 
 type ParkingSpotProps = {
-  position?: NumVec3,
   color?: string,
 };
 
+const PARKING_SPOT_POSITION: NumVec3 = [-3.6, 0, -2.1];
+
+const [x, y, z] = PARKING_SPOT_POSITION;
+
+const margin = 0.3;
+const w = CHASSIS_WIDTH + margin;
+const l = CHASSIS_LENGTH + margin;
+
+export const PARKING_SPOT_CORNERS: Array<Vector3 | [number, number, number]> = [
+  [x, y, z],
+  [x + w, y, z],
+  [x + w, y, z + l],
+  [x, y, z + l],
+];
+
 function ParkingSpot(props: ParkingSpotProps) {
-  const { position = [-3.6, 0, -2.1], color = 'yellow' } = props;
-
-  const [x, y, z] = position;
-
-  const margin = 0.3;
-  const w = CHASSIS_WIDTH + margin;
-  const l = CHASSIS_LENGTH + margin;
-
+  const { color = 'yellow' } = props;
   return (
     <Line
       points={[
-        [x, y, z],
-        [x + w, y, z],
-        [x + w, y, z + l],
-        [x, y, z + l],
-        [x, y, z],
+        ...PARKING_SPOT_CORNERS,
+        PARKING_SPOT_CORNERS[0], // Closing the line.
       ]}
       color={color}
       lineWidth={4}
