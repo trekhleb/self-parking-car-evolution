17cc5f6c87f5baf7c5a5daff140f6131bae1516a
Oleksii Trekhleb <trehleb@gmail.com>
2021-06-26 14:30:39 +0200

    Do smth.

--- src/components/world/surroundings/ParkingSpot.tsx
+++ src/components/world/surroundings/ParkingSpot.tsx
@@ -8,42 +8,75 @@ type ParkingSpotProps = {
   color?: string,
 };
 
+// @TODO: Parking lot size should be a configurable from the outside.
+// Move this constants to the component parameters.
+
+// const PARKING_SPOT_POSITION: NumVec3 = [-0.91, 0, -2];
 const PARKING_SPOT_POSITION: NumVec3 = [-3.6, 0, -2.1];
 
 const [x, y, z] = PARKING_SPOT_POSITION;
 
-const margin = 0.3;
-const w = CHASSIS_WIDTH + margin;
-const l = CHASSIS_LENGTH + margin;
+const outerW = CHASSIS_WIDTH + 0.3;
+const outerL = CHASSIS_LENGTH + 0.3;
 
-// @TODO: Parking lot size should be a configurable from the outside.
-// Move this data to the component parameters.
-const PARKING_SPOT_CORNERS: [number, number, number][] = [
-  [x + w, y, z],
-  [x + w, y, z + l],
-  [x, y, z + l],
+const innerW = 0.85 * CHASSIS_WIDTH;
+const innerL = 0.65 * CHASSIS_LENGTH;
+
+const innerX = x + (outerW - innerW) / 2;
+const innerY = y;
+const innerZ = z + (outerL - innerL) / 2;
+
+const outerCorners: [number, number, number][] = [
+  [x + outerW, y, z],
+  [x + outerW, y, z + outerL],
+  [x, y, z + outerL],
   [x, y, z],
 ];
 
+const innerCorners: [number, number, number][] = [
+  [innerX + innerW, innerY, innerZ],
+  [innerX + innerW, innerY, innerZ + innerL],
+  [innerX, innerY, innerZ + innerL],
+  [innerX, innerY, innerZ],
+];
+
 export const PARKING_SPOT_POINTS: RectanglePoints = {
-  fl: [x + w, y, z],
-  fr: [x + w, y, z + l],
-  bl: [x, y, z],
-  br: [x, y, z + l],
+  fl: [innerX + innerW, innerY, innerZ],
+  fr: [innerX + innerW, innerY, innerZ + innerL],
+  bl: [innerX, innerY, innerZ + innerL],
+  br: [innerX, innerY, innerZ],
 };
+
+const innerLineVisible = true;
 
 function ParkingSpot(props: ParkingSpotProps) {
   const { color = 'yellow' } = props;
-  return (
+
+  const innerLineComponent = innerLineVisible ? (
     <Line
       points={[
-        ...PARKING_SPOT_CORNERS,
-        PARKING_SPOT_CORNERS[0], // Closing the line.
+        ...innerCorners,
+        innerCorners[0], // Closing the line.
       ]}
       color={color}
       lineWidth={4}
       dashed={false}
     />
+  ) : null;
+
+  return (
+    <>
+      <Line
+        points={[
+          ...outerCorners,
+          outerCorners[0], // Closing the line.
+        ]}
+        color={color}
+        lineWidth={4}
+        dashed={false}
+      />
+      {innerLineComponent}
+    </>
   );
 }
 
