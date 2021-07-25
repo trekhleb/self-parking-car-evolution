baad689053b603d491fb459b81adf41ed04d229f
Oleksii Trekhleb <trehleb@gmail.com>
2021-07-25 08:52:47 +0200

    Do smth.

--- src/lib/genetic.ts
+++ src/lib/genetic.ts
@@ -4,6 +4,9 @@ export type Genome = Gene[];
 
 export type Generation = Genome[];
 
+// The number between 0 and 1.
+export type Probability = number;
+
 export type GenerationParams = {
   generationSize: number,
   genomeLength: number,
@@ -22,3 +25,43 @@ export function createGeneration(params: GenerationParams): Generation {
     .map(() => createGenome(genomeLength));
 }
 
+// Performs Uniform Crossover: each bit is chosen from either parent with equal probability.
+// @see: https://en.wikipedia.org/wiki/Crossover_(genetic_algorithm)
+export function mate(
+  father: Genome,
+  mother: Genome,
+  mutationProbability: Probability = 0.2
+): [Genome, Genome] {
+  if (father.length !== mother.length) {
+    throw new Error('Cannot mate different species');
+  }
+
+  const firstChild: Genome = [];
+  const secondChild: Genome = [];
+
+  // Conceive children.
+  for (let geneIndex = 0; geneIndex < father.length; geneIndex += 1) {
+    firstChild.push(
+      Math.random() < 0.5 ? father[geneIndex] : mother[geneIndex]
+    );
+    secondChild.push(
+      Math.random() < 0.5 ? father[geneIndex] : mother[geneIndex]
+    );
+  }
+
+  return [
+    mutate(firstChild, mutationProbability),
+    mutate(secondChild, mutationProbability),
+  ];
+}
+
+// @see: https://en.wikipedia.org/wiki/Mutation_(genetic_algorithm)
+function mutate(genome: Genome, mutationProbability: Probability): Genome {
+  // Conceive children.
+  for (let geneIndex = 0; geneIndex < genome.length; geneIndex += 1) {
+    const gene: Gene = genome[geneIndex];
+    const mutatedGene: Gene = gene === 0 ? 1 : 0;
+    genome[geneIndex] = Math.random() < mutationProbability ? mutatedGene : gene;
+  }
+  return genome;
+}
