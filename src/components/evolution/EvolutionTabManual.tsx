d3dc5b844213a9482d907e08307c0dcc7242b3e9
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-04 18:45:10 +0200

    Do smth.

--- src/components/evolution/EvolutionTabManual.tsx
+++ src/components/evolution/EvolutionTabManual.tsx
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import World from '../world/World';
-import { getSearchParam } from '../../utils/url';
 import ParkingManual from '../world/parkings/ParkingManual';
 
-const STAT_SEARCH_PARAM_NAME = 'stats';
-
 function EvolutionTabManual() {
-  const [withStat] = useState<boolean>(!!getSearchParam(STAT_SEARCH_PARAM_NAME));
-
   return (
-    <>
-      <World
-        withPerfStats={withStat}
-        withJoystickControl
-        withKeyboardControl
-      >
-        <ParkingManual withLabels withSensors />
-      </World>
-    </>
+    <World
+      withJoystickControl
+      withKeyboardControl
+    >
+      <ParkingManual
+        withLabels
+        withSensors
+      />
+    </World>
   );
 }
 
