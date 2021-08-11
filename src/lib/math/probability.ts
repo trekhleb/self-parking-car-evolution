d3a9095ca69936a2207a7fef2bb3711975413418
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-11 09:14:22 +0200

    Do smth.

---
+++ src/lib/math/probability.ts
@@ -0,0 +1,33 @@
+// Picks the random item based on its weight.
+// The items with higher weight will be picked more often.
+export const weightedRandom = <T>(items: T[], weights: number[]): T => {
+  if (items.length !== weights.length) {
+    throw new Error('Items and weights must be of the same size');
+  }
+
+  // Preparing the cumulative weights array.
+  // For example:
+  // - weights = [1, 4, 3]
+  // - cumulativeWeights = [1, 5, 8]
+  const cumulativeWeights: number[] = [];
+  for (let i = 0; i < weights.length; i += 1) {
+    cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
+  }
+
+  // Getting the random number in a range [0...sum(weights)]
+  // For example:
+  // - weights = [1, 4, 3]
+  // - maxCumulativeWeight = 8
+  // - range for the random number is [0...8]
+  const maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
+  const randomNumber = maxCumulativeWeight * Math.random();
+
+  // Picking the random item based on its weight.
+  // The items with higher weight will be picked more often.
+  for (let i = 0; i < items.length; i += 1) {
+    if (cumulativeWeights[i] >= randomNumber) {
+      return items[i];
+    }
+  }
+  return items[items.length - 1];
+};
