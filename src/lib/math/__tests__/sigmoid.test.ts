b47526c75ce1cf2dcc5b63e2477b463714eb0e36
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-17 09:04:20 +0200

    Do smth.

--- src/lib/math/__tests__/sigmoid.test.ts
+++ src/lib/math/__tests__/sigmoid.test.ts
@@ -1,4 +1,4 @@
-import { sigmoid } from '../sigmoid';
+import { sigmoid, sigmoidToCategories } from '../sigmoid';
 
 const sigmoidTestCases: [number, number][] = [
   [-100, 0],
@@ -12,12 +12,33 @@ const sigmoidTestCases: [number, number][] = [
   [100, 1],
 ];
 
+const sigmoidToCategoriesTestCases: [number, number][] = [
+  [-100, -1],
+  [-10, -1],
+  [-1, 0],
+  [-0.5, 0],
+  [0, 0],
+  [0.5, 0],
+  [1, 0],
+  [10, 1],
+  [100, 1],
+];
+
 describe('sigmoid', () => {
   it('should calculate sigmoid correctly', () => {
     for (let testCaseIndex = 0; testCaseIndex < sigmoidTestCases.length; testCaseIndex += 1) {
       const inputNumber: number = sigmoidTestCases[testCaseIndex][0];
       const expectedResult: number = sigmoidTestCases[testCaseIndex][1];
       const calculatedResult: number = Math.floor(sigmoid(inputNumber) * 100000) / 100000;
+      expect(calculatedResult).toBe(expectedResult);
+    }
+  });
+
+  it('should calculate sigmoidToCategories correctly', () => {
+    for (let testCaseIndex = 0; testCaseIndex < sigmoidToCategoriesTestCases.length; testCaseIndex += 1) {
+      const inputNumber: number = sigmoidToCategoriesTestCases[testCaseIndex][0];
+      const expectedResult: number = sigmoidToCategoriesTestCases[testCaseIndex][1];
+      const calculatedResult: number = sigmoidToCategories(sigmoid(inputNumber));
       expect(calculatedResult).toBe(expectedResult);
     }
   });
