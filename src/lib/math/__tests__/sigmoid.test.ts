f55fdcb14b4024790b773aa5cedb53dd371a422a
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-17 09:00:47 +0200

    Do smth.

---
+++ src/lib/math/__tests__/sigmoid.test.ts
@@ -0,0 +1,26 @@
+import { sigmoid } from '../sigmoid';
+
+const sigmoidTestCases: [number, number][] = [
+  [-100, 0],
+  [-10, 0.00004],
+  [-1, 0.26894],
+  [-0.5, 0.37754],
+  [0, 0.5],
+  [0.5, 0.62245],
+  [1, 0.73105],
+  [10, 0.99995],
+  [100, 1],
+];
+
+describe('sigmoid', () => {
+  it('should calculate sigmoid correctly', () => {
+    for (let testCaseIndex = 0; testCaseIndex < sigmoidTestCases.length; testCaseIndex += 1) {
+      const inputNumber: number = sigmoidTestCases[testCaseIndex][0];
+      const expectedResult: number = sigmoidTestCases[testCaseIndex][1];
+      const calculatedResult: number = Math.floor(sigmoid(inputNumber) * 100000) / 100000;
+      expect(calculatedResult).toBe(expectedResult);
+    }
+  });
+});
+
+export {};
