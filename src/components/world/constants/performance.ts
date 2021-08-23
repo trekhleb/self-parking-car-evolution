6e552b384105764bb5c1d3969f3ed447964a8861
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-23 09:28:04 +0200

    Do smth.

--- src/components/world/constants/performance.ts
+++ src/components/world/constants/performance.ts
@@ -5,10 +5,10 @@ export const INTERSECT_THROTTLE_TIMEOUT = 100;
 export const ON_RAY_THROTTLE_TIMEOUT = 100;
 
 // Throttle the onSensors sensor events.
-export const ON_SENSORS_THROTTLE_TIMEOUT = 200;
+export const ON_SENSORS_THROTTLE_TIMEOUT = 100;
 
 // Throttle the onMove events.
-export const ON_MOVE_THROTTLE_TIMEOUT = 200;
+export const ON_MOVE_THROTTLE_TIMEOUT = 100;
 
 // Throttle the update cat label events.
 export const ON_UPDATE_LABEL_THROTTLE_TIMEOUT = 600;
