adea234a839d05c076795c5f56459fe1918fb1bd
Oleksii Trekhleb <trehleb@gmail.com>
2021-08-27 02:59:44 +0200

    Do smth.

--- src/libs/math/sigmoid.ts
+++ src/libs/math/sigmoid.ts
@@ -4,7 +4,7 @@ export const sigmoid = (x: number): number => {
 
 export const sigmoidToCategories = (
   sigmoidValue: number,
-  aroundZeroMargin: number = 0.25, // Value between 0 and 1:  [0 ... (0.5 - margin) ... 0.5 ... (0.5 + margin) ... 1]
+  aroundZeroMargin: number = 0.499999, // Value between 0 and 0.5:  [0 ... (0.5 - margin) ... 0.5 ... (0.5 + margin) ... 1]
 ): -1 | 0 | 1 => {
   if (sigmoidValue < (0.5 - aroundZeroMargin)) {
     return -1;
