37e3de5d0c920060ae9b7c7eed08dc7974ef3245
Oleksii Trekhleb <trehleb@gmail.com>
2021-09-02 07:08:59 +0200

    Do smth.

--- src/components/evolution/EvolutionBoardParams.tsx
+++ src/components/evolution/EvolutionBoardParams.tsx
@@ -12,7 +12,7 @@ import FormElementsRow from '../shared/FormElementsRow';
 export const SECOND = 1000;
 
 const GENERATION_SIZES = [4, 10, 50, 100, 200, 500, 1000, 2000];
-const BATCH_SIZES = [1, 2, 5, 10, 20, 50, 100];
+const BATCH_SIZES = [1, 2, 5, 10, 15, 20, 30, 50, 100];
 
 export const DEFAULT_GENERATION_SIZE = GENERATION_SIZES[1];
 export const DEFAULT_BATCH_SIZE = BATCH_SIZES[2];
