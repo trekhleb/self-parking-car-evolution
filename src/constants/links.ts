d5f5745812d8cfa4b7f33d4239e18730dd318a21
Oleksii Trekhleb <trehleb@gmail.com>
2021-09-13 08:26:49 +0200

    Do smth.

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,4 @@
 export const TWITTER_LINK = 'https://twitter.com/Trekhleb';
 export const GITHUB_LINK = 'https://github.com/trekhleb/self-parking-car-evolution';
 export const CHECKPOINTS_PATH = 'https://github.com/trekhleb/self-parking-car-evolution/tree/master/src/checkpoints';
-// @TODO: Update the link to the article once it is ready.
-export const ARTICLE_LINK = 'https://trekhleb.dev/blog/';
+export const ARTICLE_LINK = 'https://trekhleb.dev/blog/2021/self-parking-car-evolution/';
